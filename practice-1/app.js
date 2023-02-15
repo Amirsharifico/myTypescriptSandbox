0ea89a9dca6a372f57c76f1b8cc93fab24d7a90c
amirsharifico <amirsharifico@gmail.com>
2023-02-15 12:15:53 -0500

    use common types with JS

--- practice-1/app.js
+++ practice-1/app.js
@@ -1,1 +1,5 @@
-console.log('Amirs first typescript code');
+// console.log('Amirs first typescript code')
+var site = "google.com";
+var age = 40;
+var bool = true;
+var number = 50;
--- practice-1/app.ts
+++ practice-1/app.ts
@@ -1,1 +1,14 @@
-console.log('Amirs first typescript code')
+// console.log('Amirs first typescript code')
+
+let site : string = "google.com";
+
+let age:number=40;
+
+let bool:boolean =true;
+
+const number =50;
+
+
+
+
+
